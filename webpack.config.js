a627a40f51fa3578773cbdcf8ab5814730f0a139
egiathamaershada <thama.egia@gmail.com>
2019-01-07 17:54:21 +0700

    without test

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const entryBase = function () {
+  return {
+    main: './src/js/main.js',
+  }
+}
+
+const moduleBase = function (devMode) {
+  return {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = [{
+  name: 'development',
+  mode: 'development',
+  entry: entryBase(),
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: moduleBase(true),
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/html/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}, {
+  name: 'production',
+  mode: 'production',
+  entry: entryBase(),
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: moduleBase(false),
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/html/index.html'
+    })
+  ]
+}];
