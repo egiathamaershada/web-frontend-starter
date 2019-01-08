e90c46f6f9fc3c4a212d4fbfed06a4b969736fae
egiathamaershada <thama.egia@gmail.com>
2019-01-08 21:22:55 +0700

    add simple app

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 import style from '../css/main.css'
+
+var hello = new Vue({
+  el: '#hello',
+  data: {
+    name: ''
+  },
+  computed: {
+    displayName: function() {
+      if (this.name == '') {
+        return 'World'
+      } else {
+        return this.name
+      }
+    }
+  }
+})
